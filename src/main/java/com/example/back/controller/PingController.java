71f3fb3a5ee71a7b439891c2bac6db279b82319c
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-09 18:05:48 +0900

    test for jenkins2

--- src/main/java/com/example/back/controller/PingController.java
+++ src/main/java/com/example/back/controller/PingController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PingController {
     @GetMapping("/jenkins")
     private String jenkins() {
-        return "pong";
+        return "pong!pong~pong?";
     }
 }
