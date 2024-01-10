cf0965c7c9f2362fbb4112d09250892ad66fa69c
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-10 11:27:30 +0900

    test api

---
+++ src/main/java/com/example/back/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.back.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/user/daewon")
+    public Map<String, String> getUser() {
+        Map<String, String> response = new HashMap<>();
+        response.put("name", "임대원");
+        return response;
+    }
+}
