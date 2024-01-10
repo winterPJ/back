3063e7de1f801466160c71b6378570a692b6bead
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-10 18:15:27 +0900

    cors test

--- src/main/java/com/example/back/configuration/WebConfig.java
+++ src/main/java/com/example/back/configuration/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/user/**")
+        registry.addMapping("/uuuu/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
                 .allowedHeaders("*")
