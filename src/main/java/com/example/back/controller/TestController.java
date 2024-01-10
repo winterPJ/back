ee3b211addedc96291ebdc9f664f5b315cd1d3c3
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-10 11:37:43 +0900

    test api

--- src/main/java/com/example/back/controller/TestController.java
+++ src/main/java/com/example/back/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.example.back.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
@@ -8,10 +9,13 @@ import java.util.Map;
 @RestController
 public class TestController {
 
+    @CrossOrigin(origins = "http://back.mongjo.xyz")
     @GetMapping("/user/daewon")
     public Map<String, String> getUser() {
         Map<String, String> response = new HashMap<>();
         response.put("name", "임대원");
+        response.put("age", "25");
+        response.put("job", "개발자");
         return response;
     }
 }
