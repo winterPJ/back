a7f5d4956120d5cd8b271a2c6b24da70ea1dcf5e
KorMJ <alswh456123@kyungsung.ac.kr>
2024-01-12 01:15:32 +0900

    *

--- src/main/java/com/example/back/controller/TestController.java
+++ src/main/java/com/example/back/controller/TestController.java
@@ -1,8 +1,12 @@
 package com.example.back.controller;
 
+import com.example.back.dto.UserDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -16,4 +20,13 @@ public class TestController {
         response.put("job", "개발자");
         return response;
     }
+
+    @GetMapping("/test/session")
+    public int getSession(@SessionAttribute(name = "user", required = false) UserDTO user) {
+        int result = user.getId();
+        return result;
+    }
+
+
 }
+
